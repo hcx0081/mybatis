d0e8ed20eaa8f381d9eee5ece84308335a68a8a8
root <623363564@qq.com>
2022-07-17 00:00:55 +0800

    mybatis

--- dynamicsql/src/main/java/com/mybatis/mapper/UserMapper.java
+++ dynamicsql/src/main/java/com/mybatis/mapper/UserMapper.java
@@ -2,13 +2,22 @@ package com.mybatis.mapper;
 
 
 import com.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description:
  */
 public interface UserMapper {
     List<User> findByCondition(User user);
+    
     List<User> findByIds(List<Integer> ids);
+    
+    User selectByIdAndUsername(int id, String username);
+    
+    User selectByIdAndUsernameByAnno(@Param("id") int id, @Param("username") String username);
+    
+    User selectByIdAndUsernameByMap(Map<String, Object> map);
 }
--- dynamicsql/src/test/java/com/mybatis/mapper/UserMapperTest.java
+++ dynamicsql/src/test/java/com/mybatis/mapper/UserMapperTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -36,13 +37,55 @@ class UserMapperTest {
         InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-    
+        
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-    
-        List<Integer> ids =new ArrayList<>();
+        
+        List<Integer> ids = new ArrayList<>();
         ids.add(1);
         ids.add(2);
         List<User> userList = userMapper.findByIds(ids);
         userList.forEach(System.out::println);
+    }
+    
+    @Test
+    void selectByIdAndUsernameTest() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        
+        User user = userMapper.selectByIdAndUsername(3,"wangwu");
+        
+        System.out.println(user);
+    }
+    
+    @Test
+    void selectByIdAndUsernameByAnnoTest() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        
+        User user = userMapper.selectByIdAndUsernameByAnno(3,"wangwu");
+        
+        System.out.println(user);
+    }
+    
+    @Test
+    void selectByIdAndUsernameByMapTest() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id", 3);
+        map.put("username", "wangwu");
+        User user = userMapper.selectByIdAndUsernameByMap(map);
+    
+        System.out.println(user);
     }
 }
